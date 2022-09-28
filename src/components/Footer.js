2b1bbca44cec1828c8b0d0ea8cf6f4867d9781c7
ocio <ocio@ibn5100.lokaljost>
2022-09-28 13:30:03 -0300

    ajustes, limpieza

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <a href="mailto:develop@c28.cl">develop@c28.cl</a>
       </p>
       <small style={{ marginBottom: 16 }}>Si te gustan los juegos puedes apoyar dejando tu propina con tippin</small>
-      <div id="tippin-button" data-dest="ocio28"></div>
+      <div id="tippin-button" data-dest="ocio28" style={{background: "#26282F"}}></div>
     </footer>
   );
 }
--- src/screens/Games.js
+++ src/screens/Games.js
@@ -5,6 +5,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import 'swiper/css/virtual';
+import "swiper/css/navigation";
+import { Navigation } from 'swiper';
 
 
 export default class Games extends Component {
@@ -44,6 +46,8 @@ export default class Games extends Component {
             spaceBetween={8}
             centeredSlides={true}
             onSlideChange={(e) => this.setState({ index: e.activeIndex })}
+            navigation={true} 
+            modules={[Navigation]}
           >
             {games.map((game) => (
               <SwiperSlide key={game.uuid}><img src={game.thumbnail} className="c-img c-game-thumbnail" /></SwiperSlide>
@@ -56,7 +60,7 @@ export default class Games extends Component {
             </div>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
               {actual.url.map(value => (
-                <a type="button" className="nes-btn is-success c-button-w" href={value.link} target="_blank">Jugar en {value.tipo}</a>
+                <a key={value.link} type="button" className="nes-btn is-success c-button-w" href={value.link} target="_blank">Jugar en {value.tipo}</a>
               ))}
             </div>
           </div>
