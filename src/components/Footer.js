d8e2cf5ba7b4c2fd85cc500231c1656f67c76074
ocio <ocio@ibn5100.lokaljost>
2023-01-19 19:19:42 -0300

    i18n

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import packageInfo from '../../package.json'
 
 function Footer() {
+  const { t, i18n } = useTranslation()
+
   return (
     <footer className='c-footer'>
+      <div>
+        <LangButton lang="es">Español</LangButton>
+        <LangButton lang="en">English</LangButton>
+      </div>
       <strong>{packageInfo.displayName} - {new Date().getFullYear()} v{packageInfo.version}</strong>
       <p>
         <a href="mailto:develop@c28.cl">develop@c28.cl</a>
       </p>
       <label>@ocio28</label>
-      <a href="https://t.me/papongastudio">Canal de Telegram</a>
+      <p>
+        <a href="https://t.me/papongastudio">{t("telegram")}</a>
+      </p>
+      <p>
+        {t("estilo")} <a href="https://nostalgic-css.github.io/NES.css/">NES.css</a>
+      </p>
     </footer>
   );
+}
+
+const LangButton = ({ lang, children }) => {
+  const { i18n } = useTranslation()
+  const selected = lang === i18n.resolvedLanguage
+  const className = `nes-btn ${selected ? 'is-warning' : ''} c-button-small`
+
+  const handleLanguage = () => {
+    i18n.changeLanguage(lang)
+  }
+
+  return (
+    <button className={className} onClick={handleLanguage}>{children}</button>
+  )
 }
 
 export default Footer
--- src/screens/Games.js
+++ src/screens/Games.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from 'swiper';
 
@@ -8,78 +8,87 @@ import packageInfo from '../../package.json'
 import "swiper/css";
 import 'swiper/css/virtual';
 import "swiper/css/navigation";
+import { useTranslation } from 'react-i18next';
 
 
-export default class Games extends Component {
-  state = {
-    loading: true,
-    index: 0,
-    games: []
-  }
+function Games({ width }) {
+  const { t } = useTranslation()
+  const [loading, setLoading] = useState(true)
+  const [index, setIndex] = useState(0)
+  const [games, setGames] = useState([])
 
-  componentDidMount() {
-    Api.games().then(data => {
-      this.setState({ games: data.sort(sortGames), loading: false})
-    }).catch(e => {
-      console.log(e)
-      this.setState({loading: false})
-    })
-  }
-
-  render() {
-    const { index, games } = this.state
-
-    if (games.length === 0) {
-      return
+  const fetchGames = async () => {
+    try {
+      setLoading(true)
+      const reply = await Api.games()
+      setGames(reply.sort(sortGames))
+    } catch (e) {
+      console.error("[fetchGames]", e)
+    } finally {
+      setLoading(false)
     }
+  }
 
-    const actual = games[index]
+  useEffect(() => {
+    fetchGames()
+  }, [])
 
-    return (
-      <div>
-        <div className='c-main nes-text'>
-          <div style={{ marginTop: 16, marginBottom: 32 }}>
-            <h2>{actual.titulo}</h2>
-            {actual.externo ? <small>*colaboracion</small> : null}
-          </div>
-          <Swiper
-            slidesPerView={this.props.width > 700 ? 3 : 1}
-            spaceBetween={8}
-            centeredSlides={true}
-            onSlideChange={(e) => this.setState({ index: e.activeIndex })}
-            navigation={true}
-            modules={[Navigation]}
-          >
-            {games.map((game) => (
-              <SwiperSlide key={game.uuid}><img src={game.thumbnail} className="c-img c-game-thumbnail" /></SwiperSlide>
-            ))
-            }
-          </Swiper>
-          <div style={{ marginTop: 32 }}>
-            <div style={{ minHeight: 200 }}>
-              <small>{actual.descripcion}</small>
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-              {actual.url.map(value => (
-                <a key={value.link} type="button" className="nes-btn is-success c-button-w" href={value.link} target="_blank">Jugar en {value.tipo}</a>
-              ))}
-            </div>
-          </div>
+  if (loading) {
+    return <p>...</p>
+  }
+
+  if (games.length === 0) {
+    return
+  }
+
+  const actual = games[index]
+
+  return (
+    <div>
+      <div className='c-main nes-text'>
+        <div style={{ marginTop: 16, marginBottom: 32 }}>
+          <h2>{actual.titulo}</h2>
+          {actual.externo ? <small>*{t("colaboracion")}</small> : null}
         </div>
-        <div className='c-container'>
-          <h1>{packageInfo.displayName}</h1>
-          <p>Desarrollo de videojuegos</p>
-          <small>
-            <p>Pequeño ejercito de una persona con el objetivo de hacer videojuegos</p>
-            <p>Los juegos que dicen "Jugar web" se pueden jugar en el navegador</p>
-          </small>
+        <Swiper
+          slidesPerView={width > 700 ? 3 : 1}
+          spaceBetween={8}
+          centeredSlides={true}
+          onSlideChange={(e) => setIndex(e.activeIndex)}
+          navigation={true}
+          modules={[Navigation]}
+        >
+          {games.map((game) => (
+            <SwiperSlide key={game.uuid}><img src={game.thumbnail} className="c-img c-game-thumbnail" /></SwiperSlide>
+          ))
+          }
+        </Swiper>
+        <div style={{ marginTop: 32 }}>
+          <div style={{ minHeight: 200 }}>
+            <small>{actual.descripcion}</small>
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            {actual.url.map(value => (
+              <a key={value.link} type="button" className="nes-btn is-success c-button-w" href={value.link} target="_blank">{t("play")} {value.tipo}</a>
+            ))}
+          </div>
         </div>
       </div>
-    )
-  }
+      <div className='c-container'>
+        <h1>{packageInfo.displayName}</h1>
+        <p>{t("subtitle")}</p>
+        <small>
+          <p>{t("parrafo")}</p>
+          <p>{t("nota")}</p>
+        </small>
+      </div>
+    </div>
+  )
 }
-
 
 function sortGames(a, b) {
   return a.index < b.index ? 1 : -1
 }
+
+
+export default Games
